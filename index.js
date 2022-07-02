2609e6acf5e1b7f86200e192dc51c2c466008c13
Bhairav K sharma <51754072+bhai-hub@users.noreply.github.com>
2022-07-03 00:23:34 +0530

    Add files via upload
    uploaded files

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(()=>{
+    $('.slider').slick({
+        autoplay: true,
+        arrows: true,
+        dots:true
+    }
+    );
+    $('.item-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 2
+        
+      });
+})
